ccdf07d87a20cac8f46408d8eff2166c6239dfb0
rezarahmana <rezarahmana2643@gmail.com>
2020-04-11 17:29:06 +0800

    Memanggil method dari Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ====================");
+  System.out.println("NPM  : 18630954");
+  System/out.println("Nama : Reza Rahmana");
+  System.out.println("==================================");
+  }
+}
